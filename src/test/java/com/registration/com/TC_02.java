021a5701bfda7d4c0787d652dbeededefea909ce
Samyuktha <Samyuktha@DESKTOP-TMPTRGS>
2023-05-18 15:30:09 +0530

    New Test cases added

--- src/test/java/com/registration/com/TC_02.java
+++ src/test/java/com/registration/com/TC_02.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class TC_01 {
+public class TC_02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -19,7 +19,7 @@ public class TC_01 {
 		 
 		 driver.get("http://practice.automationtesting.in/");
 		 driver.findElement(By.xpath("//a[@href=\"https://practice.automationtesting.in/my-account/\"]")).click();
-		 driver.findElement(By.name("email")).sendKeys("samyuktha2595@gmail.com");
+		 driver.findElement(By.name("email")).sendKeys("&samyukjahsfsgm.o");
 		 driver.findElement(By.id("reg_password")).sendKeys("Samyu@1995");
 		 driver.findElement(By.name("register")).click();
 
