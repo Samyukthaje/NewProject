0a5cb22a2dde64bb527706daae6017c1f2a70ef7
Samyuktha <Samyuktha@DESKTOP-TMPTRGS>
2023-05-18 14:55:50 +0530

    New Testcases added

---
+++ src/test/java/com/myaccountlogin/com/TC_05.java
@@ -0,0 +1,28 @@
+package com.myaccountlogin.com;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class TC_05 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver","F:\\SeleniumAutomation\\Softwares\\ChromeDriver\\chromedriver.exe");
+		
+		 //chrome capabilities
+		 ChromeOptions cp=new ChromeOptions();
+		 cp.addArguments("--remote-allow-origins=*");
+		 
+		 ChromeDriver driver=new ChromeDriver(cp);
+		 
+		 driver.get("http://practice.automationtesting.in/");
+		 driver.findElement(By.xpath("//a[@href=\"https://practice.automationtesting.in/my-account/\"]")).click();
+		 driver.findElement(By.name("username")).sendKeys("");
+		 driver.findElement(By.name("password")).sendKeys("");
+		 driver.findElement(By.name("login")).click();
+
+	}
+
+}
---
+++ src/test/java/com/myaccountlogin/com/TC_07.java
@@ -0,0 +1,30 @@
+package com.myaccountlogin.com;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class TC_07 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver","F:\\SeleniumAutomation\\Softwares\\ChromeDriver\\chromedriver.exe");
+		
+		 //chrome capabilities
+		 ChromeOptions cp=new ChromeOptions();
+		 cp.addArguments("--remote-allow-origins=*");
+		 
+		 ChromeDriver driver=new ChromeDriver(cp);
+		 
+		 driver.get("http://practice.automationtesting.in/");
+		 driver.findElement(By.xpath("//a[@href=\"https://practice.automationtesting.in/my-account/\"]")).click();
+		 driver.findElement(By.name("username")).sendKeys("saMYuktha2595@gmail.com");
+		 driver.findElement(By.name("password")).sendKeys("SaMYu@1995");
+		 driver.findElement(By.name("login")).click();
+		 
+		 
+
+	}
+
+}
